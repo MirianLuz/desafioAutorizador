9235bffac235821c23b0230ffd6f31b6cf3b6608
MirianLuz <61255081+MirianLuz@users.noreply.github.com>
2024-09-01 13:39:07 -0300

    initial commit

---
+++ src/main/java/com/caju/devback/Desafio/Autorizador/Transacoes/DesafioAutorizadorTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.caju.devback.Desafio.Autorizador.Transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAutorizadorTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAutorizadorTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caju/devback/Desafio/Autorizador/Transacoes/DesafioAutorizadorTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caju.devback.Desafio.Autorizador.Transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAutorizadorTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
