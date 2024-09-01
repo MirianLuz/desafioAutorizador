816105c4135baa038fff95edb93e06c74583a211
MirianLuz <61255081+MirianLuz@users.noreply.github.com>
2024-09-01 13:41:06 -0300

    initial commit

--- src/main/java/com/caju/devback/Desafio/Autorizador/Transacoes/Application.java
+++ src/main/java/com/caju/devback/Desafio/Autorizador/Transacoes/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioAutorizadorTransacoesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioAutorizadorTransacoesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
